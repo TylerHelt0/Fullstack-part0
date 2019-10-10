9667eabd0ed2c879f3932012ac06e977849566e6
Wesley Helton <wesleyrobhelton@gmail.com>
2019-10-09 19:48:30 -0500

    Rewrote setState functions correctly

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -15,13 +15,14 @@ const App = ()=>{
 
 //Set state.query when the user types a letter
   const queryChange = (event)=>{
-    setState({...state,query:event.target.value});
+    const newQuery = event.target.value
+    setState(state=>{return {...state,query:newQuery}})
   }
 
 //Set state.query to the clicked countries name
   const handleShow = (country)=>{
     return ()=>{
-      setState((state)=>{return{...state,query:country}})
+      setState(state=>{return {...state,query:country}})
     }
   }
 
@@ -32,32 +33,37 @@ const App = ()=>{
       .get(`https://restcountries.eu/rest/v2/name/${state.query}`)
       .then((res)=>{
         console.log("Contries retrieved: ", res.data)
-        setState((state)=>{return {...state,list:res.data}})
+        setState(state=>{return {...state,list:res.data}})
       })
       .catch((err)=>{
         console.log('No input')
       })
     } else if (state.query.length === 0){
-      setState((state)=>{return {...state,list:[]}})
+      setState(state=>{return {...state,list:[]}})
     }
   },[state.query])
 
 //When there is only one country to show, update state.city for showing weather
   useEffect(()=>{
     if(state.list.length === 1){
-      setState({...state,city:state.list[0].capital+', '+state.list[0].alpha2Code})
+      setState(state=>{return {...state,city:state.list[0].capital+', '+state.list[0].alpha2Code}})
     } else if (state.list.length > 1 && state.city !== ''){
-      setState({...state,city:''})
+      setState(state=>{return {...state,city:''}})
     }
   },[state.list])
 
-  //Retrieve weather city is set
+  //Retrieve weather city is set, round temps and wind speed, convert wind degree to direction
   useEffect(()=>{
     if(state.city !== ''){
       axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${state.city}&units=imperial&APPID=aee6cab34bf91e2406be1f8b2a547ced`)
       .then((res)=>{
         console.log(`Weather data for ${state.list[0].name}`,res);
         if (res.data.main && res.data.wind.speed){
+          const degToCompass = (num)=>{
+              var val = Math.floor((num / 22.5) + 0.5);
+              var arr = ["N", "NNE", "NE", "ENE", "E", "ESE", "SE", "SSE", "S", "SSW", "SW", "WSW", "W", "WNW", "NW", "NNW"];
+              return arr[(val % 16)];
+          }
           setState((state)=>{
             return {...state,
               weather:{
@@ -79,12 +85,6 @@ const App = ()=>{
       <Countries state={state} setState={setState} handleShow={handleShow}/>
     </div>
   );
-}
-
-function degToCompass(num) {
-    var val = Math.floor((num / 22.5) + 0.5);
-    var arr = ["N", "NNE", "NE", "ENE", "E", "ESE", "SE", "SSE", "S", "SSW", "SW", "WSW", "W", "WNW", "NW", "NNW"];
-    return arr[(val % 16)];
 }
 
 export default App;
